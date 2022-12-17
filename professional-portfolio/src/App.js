1c6a1ec8b32d5f3834f679eff8cc357a0e14185b
Adam Wagh <adamwagh@gmail.com>
2022-12-16 18:07:59 -0600

    welcome page done!

--- professional-portfolio/src/App.js
+++ professional-portfolio/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import HeaderDiv from './components/headerdiv';
+import Welcome from './components/pages/WelcomePage';
 
 export default function App() {
   return (
     <div>
      <header>
-      <HeaderDiv />
+      <Welcome />
      </header>
     </div>
   );
