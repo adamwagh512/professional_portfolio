bec0eae3dfbd1c6d36b09c82d3d78585e1977745
Adam Wagh <adamwagh@gmail.com>
2022-12-16 20:05:18 -0600

    got the portfolio section on its feet

--- professional-portfolio/src/App.js
+++ professional-portfolio/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import HeaderDiv from './components/headerdiv';
+import Portfolio from './components/pages/PortfolioPage';
 import Welcome from './components/pages/WelcomePage';
 
 export default function App() {
   return (
     <div>
      <header>
-      <Welcome />
+      <Portfolio />
      </header>
     </div>
   );
